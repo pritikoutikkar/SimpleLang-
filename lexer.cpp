3646143dc782763ff6114120215db14873ca73d1
pritikoutikkar <134916236+pritikoutikkar@users.noreply.github.com>
2024-07-12 20:01:49 +0530

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <cctype>
 
-struct ASTNode {
-    std::string type;
-    std::vector<ASTNode> children;
+enum TokenType { KEYWORD, OPERATOR, IDENTIFIER, LITERAL, UNKNOWN };
+
+struct Token {
+    TokenType type;
     std::string value;
 };
 
-ASTNode parse(const std::vector<Token>& tokens) {
-    ASTNode root;
-    root.type = "Program";
+std::vector<Token> tokenize(const std::string& code) {
+    std::vector<Token> tokens;
     size_t i = 0;
-    while (i < tokens.size()) {
-        if (tokens[i].type == KEYWORD && tokens[i].value == "var") {
-            ASTNode varDecl;
-            varDecl.type = "VarDecl";
-            varDecl.value = tokens[i+1].value; 
-            varDecl.children.push_back({"Literal", {}, tokens[i+3].value}); 
-            root.children.push_back(varDecl);
-            i += 4; 
-        } else {
+    while (i < code.size()) {
+        if (std::isspace(code[i])) {
             i++;
+            continue;
+        }
+        if (std::isalpha(code[i])) {
+            size_t start = i;
+            while (i < code.size() && std::isalnum(code[i])) i++;
+            std::string word = code.substr(start, i - start);
+            if (word == "var" || word == "if" || word == "else") {
+                tokens.push_back({KEYWORD, word});
+            } else {
+                tokens.push_back({IDENTIFIER, word});
+            }
+        } else if (std::isdigit(code[i])) {
+            size_t start = i;
+            while (i < code.size() && std::isdigit(code[i])) i++;
+            tokens.push_back({LITERAL, code.substr(start, i - start)});
+        } else {
+            char op = code[i++];
+            tokens.push_back({OPERATOR, std::string(1, op)});
         }
     }
-    return root;
+    return tokens;
 }
