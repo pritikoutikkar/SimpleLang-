8fc124f8189bc419da4c527480585c5a913d6b90
pritikoutikkar <134916236+pritikoutikkar@users.noreply.github.com>
2024-07-12 20:00:25 +0530

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 
-enum TokenType { KEYWORD, OPERATOR, IDENTIFIER, LITERAL, UNKNOWN };
-
-struct Token {
-    TokenType type;
+struct ASTNode {
+    std::string type;
+    std::vector<ASTNode> children;
     std::string value;
 };
 
-std::vector<Token> tokenize(const std::string& code) {
-    // Lexer implementation here
-}
-
-int main() {
-    std::string code = "var x = 10;";
-    std::vector<Token> tokens = tokenize(code);
-    for (const auto& token : tokens) {
-        std::cout << "Token: " << token.value << " Type: " << token.type << std::endl;
+ASTNode parse(const std::vector<Token>& tokens) {
+    ASTNode root;
+    root.type = "Program";
+    size_t i = 0;
+    while (i < tokens.size()) {
+        if (tokens[i].type == KEYWORD && tokens[i].value == "var") {
+            ASTNode varDecl;
+            varDecl.type = "VarDecl";
+            varDecl.value = tokens[i+1].value; 
+            varDecl.children.push_back({"Literal", {}, tokens[i+3].value}); 
+            root.children.push_back(varDecl);
+            i += 4; 
+        } else {
+            i++;
+        }
     }
-    return 0;
+    return root;
 }
