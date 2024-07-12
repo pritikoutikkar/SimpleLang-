aa8996426c18bc6e53b7ac357138493620fd9a2c
pritikoutikkar <134916236+pritikoutikkar@users.noreply.github.com>
2024-07-12 19:53:40 +0530

    Update parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 struct ASTNode {
     std::string type;
     std::vector<ASTNode> children;
+    std::string value;
 };
 
 ASTNode parse(const std::vector<Token>& tokens) {
-    // Parser implementation here
-}
-
-int main() {
-    std::vector<Token> tokens = tokenize("var x = 10;");
-    ASTNode ast = parse(tokens);
-    // Print or process the AST
-    return 0;
+    // Basic parser implementation
+    // In a complete implementation, this would be more complex
+    ASTNode root;
+    root.type = "Program";
+    size_t i = 0;
+    while (i < tokens.size()) {
+        if (tokens[i].type == KEYWORD && tokens[i].value == "var") {
+            ASTNode varDecl;
+            varDecl.type = "VarDecl";
+            varDecl.value = tokens[i+1].value; // variable name
+            varDecl.children.push_back({"Literal", {}, tokens[i+3].value}); // initial value
+            root.children.push_back(varDecl);
+            i += 4; // Skip "var x = 10"
+        } else {
+            i++;
+        }
+    }
+    return root;
 }
